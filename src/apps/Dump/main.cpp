f11a1b123917d861c8722f681d91bb6f67c137fe
Robert Habrich <r.habrich@googlemail.com>
2017-11-18 20:53:34 +0100

    Added simple tool to dump DICOM file contents

--- src/apps/Dump/main.cpp
+++ src/apps/Dump/main.cpp
@@ -1,4 +1,47 @@
+#include "Dicom/File.h"
+
+#include <iostream>
+
+using namespace dcmcpp;
+
+void dumpDataElements(const std::vector<DataElement>& dataElements)
+{
+    // TODO: maybe add a table header?
+    for (const auto& dataElement : dataElements)
+    {
+        std::cout << tagToString(dataElement.tag) << '\t';
+        std::cout << vrToString(dataElement.valueRepresentation) << '\t';
+        std::cout << std::to_string(dataElement.valueLength) << '\t';
+        std::cout << valueToString(dataElement.value) << "\t\n";
+    }
+}
+
+void dumpDicomFile(const File& dicomFile)
+{
+    std::cout << "# File Meta Information\n\n";
+    dumpDataElements(dicomFile.metaInfo().dataElements());
+    std::cout << "\n\n";
+
+    std::cout << "# Dataset\n\n";
+    dumpDataElements(dicomFile.dataset().dataElements());
+    std::cout << std::endl;
+}
+
 int main(int argc, char** argv)
 {
+    if (argc < 2)
+        return -1;
+
+    try
+    {
+        const auto dicomFile = readDicomFile(argv[1]);
+        dumpDicomFile(dicomFile);
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+
     return 0;
 }
