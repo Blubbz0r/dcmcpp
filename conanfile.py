9d47d1122f29c2d4f512c629141470dc251a78f3
Robert Habrich <r.habrich@googlemail.com>
2017-11-24 12:11:22 +0100

    GSL is now provided via conan

--- conanfile.py
+++ conanfile.py
@@ -2,5 +2,5 @@ from conans import ConanFile, CMake
 
 class PocoTimerConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "googlemock/1.7.0@Blubbz0r/stable", "google-benchmark/1.0.0@TyRoXx/stable"
+   requires = "googlemock/1.7.0@Blubbz0r/stable", "google-benchmark/1.0.0@TyRoXx/stable", "GSL/0.1@Blubb/alpha"
    generators = "cmake"
